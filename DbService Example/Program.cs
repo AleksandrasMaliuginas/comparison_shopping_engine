28f2b77fb0b2ad479425e216bdb9408b3e354f71
Simona <kassperaviciute@gmail.com>
2021-04-11 20:25:14 +0300

    Scraping to database added

--- DbService Example/Program.cs
+++ DbService Example/Program.cs
@@ -1,54 +1,131 @@
 ﻿using ComparisonEngine;
 using Domain;
-using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Globalization;
 
 namespace DbService_Example
 {
     public class Program
     {
-        static DatabaseContext context = new DatabaseContext();
-
+        static private DatabaseContext context = new DatabaseContext();
         public static void Main(string[] args)
         {
-            var product1 = new Product
+            var options = new ChromeOptions();
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            options.AddArguments("--window-size=1920,1080", "--no-sandbox", "--headless");
+
+            IWebDriver driver = new ChromeDriver(chromeDriverService, options);
+            IWebDriver driverPigu = new ChromeDriver(chromeDriverService, options);
+
+            driver.Navigate().GoToUrl("https://www.varle.lt/mobilieji-telefonai/");
+            var products = driver.FindElements(By.CssSelector("div.grid-item.product"));
+
+            foreach (var product in products)
             {
-                Title = "Test_Product2",
-                Popularity = 2
-            };
+                string[] productInfo = product.Text.Split('\n');
 
-            var attritute = new Attribute
+                var dbProduct = new Product
+                {
+                    Title = productInfo[0],
+                    Popularity = 0
+                };
+                var addedProduct = context.Products.Add(dbProduct);
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateException) { }
+
+                CreateAttribute(addedProduct.Entity, productInfo, "Gamintojas: ", "Manufacturer");
+                CreateAttribute(addedProduct.Entity, productInfo, "Ekrano įstrižainė (coliais): ", "Size");
+                CreateAttribute(addedProduct.Entity, productInfo, "Vidinė atmintis (GB): ", "Memory");
+                CreateAttribute(addedProduct.Entity, productInfo, "Atmintis (RAM) (GB): ", "Ram");
+                CreateAttribute(addedProduct.Entity, productInfo, "Ekrano raiška: ", "Resolution");
+
+                var urlVarle = product.FindElement(By.CssSelector("a.title")).GetAttribute("href");
+                var priceVarle = product.FindElement(By.CssSelector("span.price")).Text;
+                priceVarle = priceVarle.Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+                priceVarle = priceVarle.Substring(0, priceVarle.Length - 2);
+                var realProductVarle = new RealProduct
+                {
+                    AbstractProduct = addedProduct.Entity,
+                    LastCheck = DateTime.Now,
+                    Title = productInfo[0],
+                    Price = decimal.Parse(priceVarle),
+                    Url = urlVarle
+                };
+                context.RealProducts.Add(realProductVarle);
+                addedProduct.Entity.RealProducts.Add(realProductVarle);
+
+                driverPigu.Navigate().GoToUrl("https://pigu.lt/lt/search?q=" + productInfo[0]);
+                string urlPigu;
+                try
+                {
+                    var productsPigu = driverPigu.FindElement(By.CssSelector("div.product-list.all-products-visible.clearfix.product-list--equal-height"));
+                    string pricePigu = productsPigu.FindElement(By.CssSelector("span.price.notranslate")).Text.Substring(2);
+                    pricePigu = pricePigu.Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+                    string titlePigu = productsPigu.FindElement(By.CssSelector("p.product-name")).Text;
+                    urlPigu = productsPigu.FindElement(By.CssSelector("a")).GetAttribute("href");
+                    var realProducPigu = new RealProduct
+                    {
+                        AbstractProduct = addedProduct.Entity,
+                        LastCheck = DateTime.Now,
+                        Title = titlePigu,
+                        Price = decimal.Parse(pricePigu),
+                        Url = urlPigu
+                    };
+                    context.RealProducts.Add(realProducPigu);
+                    addedProduct.Entity.RealProducts.Add(realProducPigu);
+                }
+                catch (NoSuchElementException) { }
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateException) { }
+            }
+
+            driverPigu.Close();
+            driverPigu.Quit();
+            driver.Close();
+            driver.Quit();
+        }
+
+        static bool CreateAttribute(Product addedProduct, string[] productInfo, string htmlAttrName, string attrName)
+        {
+            if (Array.Find(productInfo, s => s.StartsWith(htmlAttrName)) != null)
             {
-                Name = "Attr2",
-                Value = "ValueASDF2"
-            };
+                var localAttribute = new Domain.Attribute
+                {
+                    Name = attrName,
+                    Value = Array.Find(productInfo, s => s.StartsWith(htmlAttrName)).Substring(htmlAttrName.Length).Trim()
+                };
 
-            var realProduct = new RealProduct
-            {
-                Title = "RealProd2",
-                Price = 10
-            };
+                var foundAttribute = context.Attributes.Where(a => a.Value == localAttribute.Value && a.Name == localAttribute.Name).FirstOrDefault();
+                if (foundAttribute == null)
+                {
+                    context.Attributes.Add(localAttribute);
+                    foundAttribute = localAttribute;
+                }
 
-            product1.RealProducts = new List<RealProduct>
-            {
-                realProduct
-            };
-
-            //var product2 = new Product
-            //{
-            //    Title = "Test_Product2",
-            //    Popularity = 2
-            //};
-
-            context.Products.Add(product1);
-            context.Attributes.Add(attritute);
-
-            context.ProductsAttributes.Add(new ProductAttribute
-            {
-                Product = product1,
-                Attribute = attritute
-            });
-
-            context.SaveChanges();
+                context.ProductsAttributes.Add(new ProductAttribute
+                {
+                    Product = addedProduct,
+                    Attribute = foundAttribute
+                });
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateException) { }
+                return true;
+            }
+            return false;
         }
     }
 }
